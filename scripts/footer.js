7da8d023c18d82e61120f05d040bef098dc41ccb
c-razvan <razvan.chirciu@icloud.com>
2023-11-22 11:16:00 +0200

    -footer fix

--- scripts/footer.js
+++ scripts/footer.js
@@ -84,6 +84,7 @@ let date = new Date();
 let copyRight = document.createElement("div");
 copyRight.style.textAlign = "center";
 copyRight.style.padding = "1rem";
+copyRight.style.marginBottom = "1rem";
 copyRight.style.backgroundColor = "rgb(240,240,240)";
 copyRight.innerHTML = `Federația Română de Gimnastică ${date.getFullYear()} - Toate drepturile rezervate`;
 
