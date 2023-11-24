4293c7ffdd2cbb807ea9c0db4a775a834c7e9220
c-razvan <razvan.chirciu@icloud.com>
2023-11-24 12:58:37 +0200

    -save

--- scripts/static scripts/intrebari.js
+++ scripts/static scripts/intrebari.js
@@ -1,92 +1,51 @@
-let buttons = [
-  {
-    title: "Redirecționează",
-    color: "blue",
-    content: {
-      linkTitle: ["3,5% - Persoane Fizice", "20% - Persoane Juridice"],
-      links: ["./", "./"],
-    },
-  },
-  {
-    title: "Donează",
-    color: "yellow",
-    content: {
-      linkTitle: ["Persoane Fizice", "Persoane Juridice"],
-      links: ["./", "./"],
-    },
-  },
-  {
-    title: "Contracte",
-    color: "red",
-    content: {
-      linkTitle: ["Sponsorizare în Bani", "Sponsorizare în Bunuri și Servicii"],
-      links: ["./", "./"],
-    },
-  },
-];
-
-for (let j = 0; j < buttons.length; j++) {
-  // button container
-  let buttonContainer = document.createElement("div");
-  buttonContainer.setAttribute("class", "button-container");
-
-  // button
-  let button = document.createElement("button");
-  button.setAttribute(
-    "class",
-    `collapsible mt-3 buton-redirectionare button-select-${buttons[j].color}`
-  );
-  button.setAttribute("type", "button");
-  button.innerHTML = `${buttons[j].title}`;
-
-  let indicator = document.createElement("img");
-  indicator.setAttribute(
-    "src",
-    "./assets/icon/arrow.uturn.down.circle.fill.svg"
-  );
-  indicator.setAttribute("class", "indicator");
-
-  // content
-  let content = document.createElement("div");
-  content.setAttribute("class", "content mt-3 hidden-card");
-  content.setAttribute("id", `${j}`);
-
-  for (let h = 0; h < buttons[j].content.links.length; h++) {
-    // linkContainer
-    let linkContainer = document.createElement("div");
-    linkContainer.setAttribute("class", "link-container");
-
-    // link
-    let link = document.createElement("a");
-    link.setAttribute("href", `${buttons[j].content.links[h]}`);
-    link.innerHTML = `${buttons[j].content.linkTitle[h]}`;
-    linkContainer.appendChild(link);
-    content.appendChild(linkContainer);
-  }
-
-  //append
-  button.appendChild(indicator);
-  buttonContainer.appendChild(button);
-  buttonContainer.appendChild(content);
-  document.getElementById("buttons-container").appendChild(buttonContainer);
-}
-
-let indicator = document.getElementsByClassName("indicator");
-let coll = document.getElementsByClassName("collapsible");
+let acc = document.getElementsByClassName("accordion");
 let i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    let content = this.nextElementSibling;
-    if (content.classList.contains("hidden-card")) {
-      indicator[content.id].classList.add("rotate");
-      content.classList.remove("hidden-card");
-      content.classList.add("show-card");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
     } else {
-      indicator[content.id].classList.remove("rotate");
-      content.classList.add("hidden-card");
-      content.classList.remove("show-card");
+      panel.style.display = "block";
     }
   });
+}
+
+
+
+let section = document.createElement("div");
+section.setAttribute("class", "w-100 mb-5");
+
+let sectionTitle = document.createElement("h2");
+sectionTitle.innerHTML = "Campanii";
+
+//append title
+
+section.appendChild(sectionTitle) 
+
+for (let j = 0; j < 3; j++) {
+
+  let sectionButton = document.createElement("button");
+  sectionButton.setAttribute("class", "accordion");
+  sectionButton.innerHTML = "Q?"
+
+  let panel = document.createElement("panel");
+  panel.setAttribute("class", "panel");
+
+  let answer = document.createElement("p");
+  answer.innerHTML = `
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+  nisi ut aliquip ex ea commodo consequat.`;
+
+  //append
+
+  panel.appendChild(answer)
+  section.appendChild(sectionButton)
+  section.appendChild(panel)
+
+  document.getElementById("QA").appendChild(section)
 }
