b80d03313e2c378b1ced63402322d17ade72b085
c-razvan <razvan.chirciu@icloud.com>
2023-11-24 16:09:05 +0200

    -QA

--- scripts/static scripts/intrebari.js
+++ scripts/static scripts/intrebari.js
@@ -1,3 +1,67 @@
+const QA = [
+  {
+    title: "Campanii",
+    question: [
+      "În ce scop sunt folosite Donațiile?",
+      "Când va fi disponibilă campania de Redirecționare?",
+      "Pot propune o Campanie?",
+    ],
+    answer: [
+      "Donațiile sunt folosite în funcție de cauza spre care alegeți să redirecționați suma de bani alocată. Împreună dezvoltăm gimnastica românească!",
+      "Redirecționarea unui procent din impozitul pe venit este cea mai bună metodă prin care puteti ajuta gimnaștii români. Campania pentru redirecționare va începe în anul 2022, iar formularul pentru aceasta va putea fi completat și finalizat exclusiv online.",
+      "Puteți propune o cauză cu un e-mail la donatii@frgimnastica.com , iar noi vă vom contacta ulterior.",
+    ],
+  },
+  {
+    title: "Ajutor",
+    question: [
+      "Cum donez?",
+      "Când va fi disponibilă campania de Redirecționare?",
+      "Pot propune o Campanie?",
+    ],
+    answer: [
+      "La secțiunea `donează` veți regăsi proiectele în derulare. Puteți dona către orice proiect în care v-ar face plăcere să vă implicați și să ajutați accesând pagina acestuia. Fiecare proiect are o cauză și un obiectiv independent, iar dumneavoastră sunteți persoana care alege unde doriți să direcționați fondurile alocate donației.",
+      "Datele și securitatea dumneavoastră sunt foarte importante pentru noi! Lucrăm în parteneriat cu procesorul de plăți securizate EuroPayment Services (euplatesc.ro) care asigură siguranță datelor la nivel de sistem informatic și are la dispoziție metode adecvate de securitate în conformitate cu protecția datelor cu caracter personal, a tranzacțiilor și a operațiunilor pe care utilizatorii le efectuează.",
+      "În cazul în care întâmpinați alte probleme, vă rugăm se ne contactați. Dacă întâmpinați orice alt tip de dificultate în finalizarea unei donații sau aveți alte întrebări legate de acestea, vă rugăm să ne contactați la adresa de e-mail donatii@frgimnastica.com",
+    ],
+  },
+];
+
+//logic
+
+for (let h = 0; h < QA.length; h++) {
+  let state = QA[h];
+  let section = document.createElement("div");
+  section.setAttribute("class", "w-100 mb-5");
+
+  let sectionTitle = document.createElement("h3");
+  sectionTitle.setAttribute("class", "ms-4");
+  sectionTitle.innerHTML = state.title;
+
+  //append title
+  section.appendChild(sectionTitle);
+
+  for (let j = 0; j < state.question.length; j++) {
+    let sectionButton = document.createElement("button");
+    sectionButton.setAttribute("class", "accordion");
+    sectionButton.innerHTML = state.question[j];
+
+    let panel = document.createElement("div");
+    panel.setAttribute("class", "panel mt-3");
+
+    let answer = document.createElement("p");
+    // answer.setAttribute("class","ms-3")
+    answer.innerHTML = state.answer[j];
+
+    //append
+    panel.appendChild(answer);
+    section.appendChild(sectionButton);
+    section.appendChild(panel);
+  }
+
+  document.getElementById("QA").appendChild(section);
+}
+
 let acc = document.getElementsByClassName("accordion");
 let i;
 
@@ -11,41 +75,4 @@ for (i = 0; i < acc.length; i++) {
       panel.style.display = "block";
     }
   });
-}
-
-
-
-let section = document.createElement("div");
-section.setAttribute("class", "w-100 mb-5");
-
-let sectionTitle = document.createElement("h2");
-sectionTitle.innerHTML = "Campanii";
-
-//append title
-
-section.appendChild(sectionTitle) 
-
-for (let j = 0; j < 3; j++) {
-
-  let sectionButton = document.createElement("button");
-  sectionButton.setAttribute("class", "accordion");
-  sectionButton.innerHTML = "Q?"
-
-  let panel = document.createElement("panel");
-  panel.setAttribute("class", "panel");
-
-  let answer = document.createElement("p");
-  answer.innerHTML = `
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-  enim ad minim veniam, quis nostrud exercitation ullamco laboris
-  nisi ut aliquip ex ea commodo consequat.`;
-
-  //append
-
-  panel.appendChild(answer)
-  section.appendChild(sectionButton)
-  section.appendChild(panel)
-
-  document.getElementById("QA").appendChild(section)
 }
